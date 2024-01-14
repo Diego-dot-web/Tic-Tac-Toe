13015c62f584e703830be8a29bb41b92fdfea6de
Diego <134121369+Diego-dot-web@users.noreply.github.com>
2024-01-13 22:49:43 -0500

    Start skeleton of the project

---
+++ script.js
@@ -0,0 +1,15 @@
+(function Gameboard(){
+    const gameboard = [];
+    const colums = 3;
+    const rows = 3;
+
+    //this create the game, and store the results
+
+    //The players
+    const players = [
+        {
+            playerOne: ""
+        }
+    ]
+    //the flow
+})();
